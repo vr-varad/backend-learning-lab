26a398bdcf90cd5d30d296a04bc736deee3dda14
vr-varad <varadgupta21@gmail.com>
2024-10-18 21:16:15 +0530

    project-3: Making proccess Scalable using Cluster Module
    Signed-off-by: vr-varad <varadgupta21@gmail.com>

--- project-3-book-api/server.js
+++ project-3-book-api/server.js
@@ -1,12 +1,29 @@
 const express = require("express");
 require("express-async-errors");
+
+const cluster = require("cluster");
+const os = require("os");
 const Server = require("./src");
 
 const port = process.env.PORT || 8080;
+const numCPUs = os.cpus().length;
 
-const app = express();
-const server = new Server(app);
+if (cluster.isPrimary) {
+  console.log(`Master process is running with PID: ${process.pid}`);
 
-app.listen(port, "localhost", () => {
-  console.log(`Server is running on port ${port}`);
-});
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died. Restarting...`);
+    cluster.fork();
+  });
+} else {
+  const app = express();
+  const server = new Server(app);
+
+  app.listen(port, "localhost", () => {
+    console.log(`Worker ${process.pid} running server on port ${port}`);
+  });
+}
