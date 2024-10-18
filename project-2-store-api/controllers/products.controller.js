bb9af763fb3b5470f1063a995a26bd39d005accd
vr-varad <varadgupta21@gmail.com>
2024-10-18 17:01:34 +0530

    project-2: Added GetAllProducts With Multiple Features
    Signed-off-by: vr-varad <varadgupta21@gmail.com>

--- project-2-store-api/controllers/products.controller.js
+++ project-2-store-api/controllers/products.controller.js
@@ -1,5 +1,57 @@
+const Product = require("../models/products.model");
+
 const getAllProducts = async (req, res) => {
-    throw new Error("There is an error!");
+  const { featured, name, company, sort, field, numericFilters } = req.query;
+  const queryObject = {};
+  console.log(numericFilters);
+  if (featured) queryObject.featured = featured;
+  if (company) queryObject.company = company;
+  if (name) queryObject.name = { $regex: name, $options: "i" };
+  if (numericFilters) {
+    const operators = {
+      "<": "$lt",
+      "<=": "$lte",
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+    };
+    const regEX = /\b(<|>|<=|>=|=)\b/g;
+    let filters = String(numericFilters).replace(
+      regEX,
+      (match) => `-${operators[match]}-`
+    );
+    const fields = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = String(item).split("-");
+      if (fields.includes(field)) {
+        queryObject[field] = { [operator]: value };
+      }
+    });
+  }
+  let results = Product.find(queryObject);
+
+  if (sort) {
+    const sortBy = String(sort).split(",").join(" ");
+    results.sort(sortBy);
+  } else {
+    results.sort("createdAt");
+  }
+
+  if (field) {
+    const fields = String(field).split(",").join(" ");
+    results.select(fields);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.page) || 10;
+  const skip = (page - 1) * limit;
+  results = results.skip(skip).limit(limit);
+
+  const products = await results;
+  res.status(200).json({
+    products,
+    nProducts: products.length,
+  });
 };
 
 module.exports = {
--- project-2-store-api/middlewares/errorHandler.js
+++ project-2-store-api/middlewares/errorHandler.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
+  console.log(err)
   return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
     msg: "Something went wrong, please try again.",
   });
